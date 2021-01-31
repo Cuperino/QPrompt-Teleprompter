c82d35394867a632e785500170643295754915b0
Javier O. Cordero Pérez <javier@imaginary.tech>
2021-01-31 15:35:11 -0400

    A few path fixes in build scripts... Expanded CMake with a few potential solutions to compilation problems; commented out lines that prevented compiling for other architectures.

--- 3rdparty/build-dependencies.sh
+++ 3rdparty/build-dependencies.sh
@@ -8,12 +8,29 @@ export ANT=/usr/bin/ant
 export JAVA_HOME=/home/javier/Software/JAVA/jdk-11.0.10+9
 export CMAKE_PREFIX_PATH=/home/javier/Qt/5.15.2/Src/:$CMAKE_PREFIX_PATH
 
+# Cleanup everything
+rm -dRf ./*/build
+
+# ECM
+mkdir extra-cmake-modules/build
+cd extra-cmake-modules/build
+cmake ..
+make
+sudo make install
+
+cd ../../
+# Kirigami
 mkdir kirigami/build
 cd kirigami/build
 cmake .. \
-    -DCMAKE_TOOLCHAIN_FILE=/usr/share/ECM/toolchain/Android.cmake \
-    -DCMAKE_PREFIX_PATH=/home/javier/Qt/5.15.2/Src/qtbase/\
-    -DCMAKE_INSTALL_PREFIX=/home/javier/path-to-dummy-install-prefix\
-    -DECM_DIR=/path/to/share/ECM/cmake\
+    -DCMAKE_TOOLCHAIN_FILE=/usr/local/share/ECM/toolchain/Android.cmake \
+    -DCMAKE_PREFIX_PATH=/home/javier/Qt/5.15.2/Src/qtbase/ \
+    -DCMAKE_INSTALL_PREFIX=/home/javier/Development/Teleprompters/qprompt/QPrompt/3rdparty/bin \
+    -DECM_DIR=/usr/local/share/ECM/cmake
 make
-make install
+sudo make install
+# cmake .. \
+#     -DCMAKE_TOOLCHAIN_FILE=/usr/share/ECM/toolchain/Android.cmake \
+#     -DCMAKE_PREFIX_PATH=/home/javier/Qt/5.15.2/Src/qtbase/\
+#     -DCMAKE_INSTALL_PREFIX=/local/usr/bin\
+#     -DECM_DIR=/usr/share/ECM/cmake/\
