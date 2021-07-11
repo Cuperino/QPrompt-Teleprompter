0e995528ad03763ac94cefbf0989fcb4f133ceec
Javier O. Cordero Pérez <javier@imaginary.tech>
2021-07-11 01:46:48 -0400

    Added building sript to be used in conjunction with KDE's Craft.

---
+++ dist/CraftRoot/etc/blueprints/locations/craft-blueprints-kde/kde/applications/qprompt/qprompt.py
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2021 Javier O. Cordero Pérez
+
+import info
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.displayName = "QPrompt"
+        self.description = "Personal teleprompter software"
+        self.webpage = "https://qprompt.app"
+        self.svnTargets['main'] = 'git@github.com:Cuperino/QPrompt.git|main'
+        self.defaultTarget = "main"
+        # self.versionInfo.setDefaultValues()  # Unused, depends on KDE infrastructure
+    def setDependencies(self):
+        # self.buildDependencies["virtual/base"] = None
+        # self.buildDependencies["dev-utils/appimagetool"] = None
+        self.buildDependencies["dev-utils/linuxdeploy"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/qt5/qtdeclarative"] = None
+        self.runtimeDependencies["libs/qt5/qtandroidextras"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
