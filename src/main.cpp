78bbedb84c06f08111279bcd0c951c0c3ff2086c
Javier O. Cordero Pérez <javier@imaginary.tech>
2021-07-17 17:31:37 -0400

    Fixed path missing icon on main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -120,7 +120,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 
     // Un-comment to force RightToLeft Layout for debugging purposes
     //app.setLayoutDirection(Qt::RightToLeft);
-    app.setWindowIcon(QIcon(":/images/logo.png"));
+    app.setWindowIcon(QIcon(":/images/qprompt.png"));
     
 //     MarkersModel markersModel;
     engine.rootContext()->setContextObject(new KLocalizedContext(&engine));
