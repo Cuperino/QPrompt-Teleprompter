34192f0da71dc2edb163a3885090ecf87871830f
Javier O. Cordero Pérez <javier@imaginary.tech>
2021-10-06 19:36:30 -0400

    Fixed graphics permissions on Flatpak build. Fixed network permissions on Snap build. Re-enabled versionInfo for Craft now that CMake is tied to git versioning. Loading of platform specific dependencies now is only attempted on the respective platforms, reducing file sizes.

--- dist/CraftRoot/etc/blueprints/locations/craft-blueprints-kde/kde/applications/qprompt/qprompt.py
+++ dist/CraftRoot/etc/blueprints/locations/craft-blueprints-kde/kde/applications/qprompt/qprompt.py
@@ -2,26 +2,35 @@
 # SPDX-FileCopyrightText: 2021 Javier O. Cordero Pérez
 
 import info
+from sys import platform
 
 class subinfo(info.infoclass):
     def setTargets(self):
+        self.versionInfo.setDefaultValues()
         self.displayName = "QPrompt"
         self.description = "Personal Teleprompter Software"
         self.webpage = "https://qprompt.app"
         self.svnTargets['main'] = 'git@github.com:Cuperino/QPrompt.git|main'
         self.defaultTarget = "main"
-        # self.versionInfo.setDefaultValues()  # Unused, depends on KDE infrastructure
+
     def setDependencies(self):
-        # self.buildDependencies["virtual/base"] = None
-        # self.buildDependencies["dev-utils/appimagetool"] = None
-        self.buildDependencies["dev-utils/linuxdeploy"] = None
+        # Assuming host platform is destination platform
+        self.buildDependencies["virtual/base"] = None
+        if platform == "linux":
+            self.buildDependencies["dev-utils/linuxdeploy"] = None
+            self.buildDependencies["dev-utils/appimagetool"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt5/qtsvg"] = None
         self.runtimeDependencies["libs/qt5/qtbase"] = None
         self.runtimeDependencies["libs/qt5/qtdeclarative"] = None
-        self.runtimeDependencies["libs/qt5/qtandroidextras"] = None
+        self.runtimeDependencies["libs/qt5/qtquickcontrols"] = None
+        self.runtimeDependencies["libs/qt5/qtquickcontrols2"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
+        if CraftCore.compiler.isAndroid:
+            self.runtimeDependencies["libs/qt5/qtandroidextras"] = None
 
 from Package.CMakePackageBase import *
 
