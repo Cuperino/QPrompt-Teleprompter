c4b3ad2bc0c2ee7a9f526cfc87311126c23985fa
Javier O. Cordero Pérez <javier@imaginary.tech>
2021-10-12 02:24:51 -0400

    Removed Android registerTypes because it doesn't seem to be required when using ECM for Android builds.

--- src/main.cpp
+++ src/main.cpp
@@ -112,9 +112,9 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     QQmlFileSelector::get(&engine)->setExtraSelectors(selectors);
-    #ifdef Q_OS_ANDROID
-    KirigamiPlugin::getInstance().registerTypes();
-    #endif
+    // #ifdef Q_OS_ANDROID
+    // KirigamiPlugin::getInstance().registerTypes();
+    // #endif
 
     // Un-comment to force RightToLeft Layout for debugging purposes
     //app.setLayoutDirection(Qt::RightToLeft);
