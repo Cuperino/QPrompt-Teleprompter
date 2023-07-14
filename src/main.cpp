53c56a71acceb7492e8a80db70c4be207c350666
Javier O. Cordero Pérez <javiercorderoperez@gmail.com>
2023-07-13 21:57:11 -0400

    Removed unnecessary qputenv line left by mistake on previous commit

--- src/main.cpp
+++ src/main.cpp
@@ -104,13 +104,13 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 
 // The following code forces the use of specific renderer modes to enable screen projections to work.
 // This hacky must be completely
-#if defined(Q_OS_WINDOWS)|| defined(Q_OS_MACOS) || defined(Q_OS_LINUX)
+#if defined(Q_OS_WINDOWS) || defined(Q_OS_MACOS) || defined(Q_OS_LINUX)
     if (enableProjections.toBool())
 #if defined(Q_OS_WINDOWS)
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
         qputenv("QSG_RENDER_LOOP", "windows");
 #else
-    qputenv("QSG_RENDER_LOOP", "basic");
+        qputenv("QSG_RENDER_LOOP", "basic");
 #endif
     else
         qputenv("QSG_RENDER_LOOP", "threaded");
@@ -118,7 +118,6 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
       // On *nix, setenv needs to override QSG_RENDER_LOOP for it to take effect after qprompt automatically restarts.
       // By default, we do not override environment variables. qgsIgnore is set by the code doing the restart.
         setenv("QSG_RENDER_LOOP", "basic", parser.isSet(qgsIgnore) ? 1 : 0);
-        qputenv("QSG_RENDER_LOOP", "basic");
 #if defined(Q_OS_LINUX)
     // Mac does not currently support threaded mode, forcing it crashes the app on startup.
     else
