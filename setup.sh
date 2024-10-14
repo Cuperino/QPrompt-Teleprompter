6e79b3d94f6c196139255a357e5d5fc420d40074
Javier O. Cordero Pérez <javier.cordero@kdab.com>
2024-10-14 09:25:24 -0400

    Bump Qt version

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@
 #**************************************************************************
 
 ARCHITECTURE="$(uname -m)"
-QT_VER=6.7.2
+QT_VER=6.7.3
 echo -e "\nArchitecture: $ARCHITECTURE"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
