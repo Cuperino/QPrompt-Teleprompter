13710ca9f36e7a16ea78671e86176378bcec8f9b
Javier O. Cordero Pérez <javiercorderoperez@gmail.com>
2025-06-27 23:47:00 -0400

    Automatic export of non-plural English translations

--- po/qprompt_en.ts
+++ po/qprompt_en.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US" sourcelanguage="en_US">
+<TS version="2.1" language="en" sourcelanguage="en">
 <context>
     <name>Find</name>
     <message numerus="yes">
