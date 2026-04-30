3b9d9570ffe71ad61bfa546da94d39b8af3f07c9
Javier O. Cordero Pérez <javiercorderoperez@gmail.com>
2026-04-30 12:28:31 -0400

    Correct copyright header details

--- src/spellchecker.cpp
+++ src/spellchecker.cpp
@@ -1,13 +1,21 @@
 /****************************************************************************
  **
  ** QPrompt
- ** Copyright (C) 2020-2026 Javier O. Cordero Pérez
+ ** Copyright (C) 2026 Javier O. Cordero Pérez
  **
  ** This file is part of QPrompt.
  **
  ** This program is free software: you can redistribute it and/or modify
  ** it under the terms of the GNU General Public License as published by
  ** the Free Software Foundation, version 3 of the License.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
  **
  ****************************************************************************/
 
