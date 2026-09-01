650ff53414da4c2cfe074bc05d68971f6f506a95
Javier O. Cordero Pérez <javiercorderoperez@gmail.com>
2026-09-01 02:06:53 +0000

    Added translation using Weblate (Mongolian)

---
+++ po/qprompt_mn.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="mn" sourcelanguage="en">
+<context>
+    <name>Find</name>
+    <message numerus="yes">
+        <location filename="../../../../src/prompter/Find.qml" line="160"/>
+        <source>Replaced %n instance(s)</source>
+        <translation type="unfinished">
+            <numerusform>Replaced %n instance</numerusform>
+            <numerusform>Replaced %n instances</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>PrompterPage</name>
+    <message numerus="yes">
+        <location filename="../../../../src/kirigami_ui/PrompterPage.qml" line="899"/>
+        <source>Disappear within %n second(s) to go</source>
+        <translation type="unfinished">
+            <numerusform>Disappear within %n second to go</numerusform>
+            <numerusform>Disappear within %n seconds to go</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
